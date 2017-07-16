513431ae93a4b1231ef915491ab2421b0f6576e0
唐学智 <tangxuezhi@MacBook-Pro.local>
2017-07-16 22:55:24 +0800

    add koa-favicon

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 const Koa = require('koa');
-const Router = require('koa-router');
+const favicon = require('koa-favicon');
 
 const app = new Koa();
 
 const {logger, log4js} = require('./utils/logger');
-const router = require('./routes')
+const router = require('./routes');
 
+// not work correct ?
+app.use(favicon(__dirname + '/favicon.ico'));
+
+// listen to http log
 app.use(log4js.koaLogger(log4js.getLogger("http"), { level: 'auto' }));
 
 app.use(router.routes());
